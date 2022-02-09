c0d94bd27def15aecb2204392ec4e9e0f8f6a52c
adam-camp <98061122+adam-camp@users.noreply.github.com>
2022-02-09 14:14:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "topo-vector"
+require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/ImageryLayer",
+        "esri/layers/support/RasterFunction"
+      ], (Map, MapView, ImageryLayer, RasterFunction) => {
+        /***************************************
+         * Set up popup template of image layer
+         **************************************/
+
+        const imagePopupTemplate = {
+          // autocasts as new PopupTemplate()
+          title: "NDVI DATA",
+          content: `
+<b>{Raster.NDVI Colorized} </b>
+            `
+        };
+
+        /*******************************************************************
+         * Create image layer with server defined raster function templates
+         ******************************************************************/
+
+        const serviceRFT = new RasterFunction({
+          functionName: "NDVI Colorized",
+          variableName: "Raster"
         });
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
+        const layer = new ImageryLayer({
+          url: "https://landsat2.arcgis.com/arcgis/rest/services/Landsat8_Views/ImageServer",
+          renderingRule: serviceRFT,
+          popupTemplate: imagePopupTemplate
         });
-      });
+
+        /*************************
+         * Add image layer to map
+         ************************/
+
+        const map = new Map({
+          basemap: "hybrid",
+          layers: [layer]
+        });
+
+        const view = new MapView({
+          container: "viewDiv",
+          center: [-90.18, 38.672],
+           zoom: 6,
+           map: map
+          });
+});
